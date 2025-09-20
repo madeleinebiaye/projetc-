434bd1b850fae1d748ed71c09f982548c81c4ccc
Diago Alioune Tall <diagoalioune.tall@gmail.com>
2025-09-20 19:51:12 +0000

    testing visual

---
+++ main/main.c
@@ -0,0 +1,21 @@
+#include "visual.h"
+
+int main() {
+    if (!init_SDL()) return 1;
+
+    int tableau[] = {5, 2, 8, 3, 7}; // tableau fixe
+    int taille = 5;
+
+    afficher_tableau(tableau, taille);
+
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) quit = 1;
+        }
+    }
+
+    fermer_SDL();
+    return 0;
+}
